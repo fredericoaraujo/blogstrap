e3208af949a7a593dc08fe1b170968638aa10429
Frederico Araujo <fredericoaraujo@gmail.com>
2021-09-25 16:37:15 -0300

    atualiza artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = 'Article was successfully updated'
+      redirect_to @article
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
