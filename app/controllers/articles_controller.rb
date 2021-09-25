70ceccaa981cb4913b508594e6bb13691cec57ee
Frederico Araujo <fredericoaraujo@gmail.com>
2021-09-25 16:01:26 -0300

    Adicionando formulario de novo artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Article successfully created'
+      redirect_to @article
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
