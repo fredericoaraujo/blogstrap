3bfded2f33092b40f4492746d422eff70b7ddb22
Frederico Araujo <fredericoaraujo@gmail.com>
2021-09-27 20:35:35 -0300

    Refatorando category

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,5 @@ class Article < ApplicationRecord
 
   scope :desc_order, -> { order(created_at: :desc) }
   scope :wthout_highlights, ->(ids) { where("id NOT IN(#{ids})") if ids.present? }
+  scope :filter_by_category, ->(category_id) { where category_id: category_id if category_id.present? }
 end
