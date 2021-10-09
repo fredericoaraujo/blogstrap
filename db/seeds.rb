ae729158809bec2dbe5538bdea889436a0d12a93
Frederico Araujo <fredericoaraujo@gmail.com>
2021-10-09 17:41:42 -0300

    Criado seed para popular o projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p 'Início'
+Article.delete_all
+
+amount_user = User.all.count
+
+Category.all.each do |category| 
+    30.times do
+        user_id = rand(1..amount_user)
+        amount = rand(30)
+        amount.times do
+            Article.create!(
+                title: Faker::Quote.famous_last_words,
+                body: Faker::Lorem.paragraph(sentence_count: rand(10..1_000)),
+                user_id: user_id,
+                category: category,
+                created_at: rand(365).days.ago
+            )
+        end
+    end
+end
+
+p 'Fim...'
